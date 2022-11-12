3b4deac3e399fd3c5f29899e52f22a09b7099ea4
Adam Lopez <adamf04@gmail.com>
2022-11-12 11:22:50 -0500

    adding controller functions that make calls to the modal to obtain data from the albums

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    public function index() {
+        $albums = Album::latest()->paginate(50);
+        return view('home', compact('albums'));
+    }
+
+    public function userAlbum($id) {
+        $albums = Album::where('user_id', $id)->get();
+        return view('user-album', compact('albums'));
+    }
+}
