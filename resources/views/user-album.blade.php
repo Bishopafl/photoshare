cde4e89f795b886a2ce5bf752ed8ebff030ea370
Adam Lopez <adamf04@gmail.com>
2022-11-12 11:24:48 -0500

    view for user-album route

---
+++ resources/views/user-album.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="mb-3">
+        <img src="{{ asset('banner') }}/gojo.png" style="width: 100%" alt="">
+    </div>
+    
+
+    <div class="row">
+        @foreach($albums as $album)
+            
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="{{ asset('album') }}/{{ $album->image }}" class="card-img-top" alt="">
+                    </div>
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <center>{{ $album->name }}</center>
+                        </h5>
+                        <center>
+                            <a href="{{ route('view.album', [$album->slug, $album->id]) }}" class="btn btn-primary">View Album</a>
+                        </center>
+                    </div>
+                </div>    
+            </div>
+
+        @endforeach
+    </div>
+</div>
+@endsection
