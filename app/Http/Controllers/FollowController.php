71f150adc0b7a3614acb490be006e40b50f4e547
Adam Lopez <adamf04@gmail.com>
2022-11-14 23:02:27 -0500

    follower relationship and the follower controller passing eloquent variables to the front end

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Follower;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,19 @@ class FollowController extends Controller
         $followingId = User::find($requst->userId);
         $followerId->following()->toggle($followingId);
         return redirect()->back();
+    }
+
+    /**
+     * get followers where follower_id is 
+     * equal to logged in user and return 
+     * it to the view
+     */
+    public function profile() {
+        $followings = Follower::where('follower_id', auth()->user()->id)->get();
+        foreach ($followings as $following) {
+            $userId = $following->userfollow->id;
+            $follows = (new User)->amIfollowing($userId);
+        }
+        return view('profile', compact('userId', 'follows', 'followings'));
     }
 }
--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -9,4 +9,16 @@ class Follower extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    /**
+     * All followers belong to a user
+     * 
+     * @param int $following_id
+     * @param int $id
+     * 
+     * @return $following_id
+     */
+    public function userfollow() {
+        return $this->belongsTo(User::class, 'following_id', 'id');
+    }
 }
